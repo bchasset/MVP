d352af5a0c5e54dd2dbecd4f30872defdf7eb5b8
Brian Hassett <bchasset@gmail.com>
2017-01-23 17:05:45 -0800

    Testing mongodb -- working locally

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
-
-//mongoose.connect('mongodb://localhost/workout')
-
+var Q = require('q');
+var bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/workout');
+
 
 app.listen(process.env.PORT || 3000);
 
-// app.get('/', function() {
-//   res.send()
-// })
+
+
+var testSchema = mongoose.Schema({
+  name: String
+});
+
+var Test = mongoose.model('Test', testSchema);
+
+app.post('/testingtesting', function(req, res) {
+  console.log(req.body.name);
+  var thing = new Test({
+    name: req.body.name
+  });
+  thing.save(function(err, thing) {
+    if(err) {
+      return console.error(err);
+    } else {
+      res.status(200).send('success');
+    }
+  })
+})
+
+app.get('/testingtesting', function(req, res) {
+  Test.find(function(err, tests) {
+    res.status(200).send(tests);
+  })
+})
