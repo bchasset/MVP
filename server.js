b372ea337e9a6fcce896b42c976bb874f1bcfa75
Brian Hassett <bchasset@gmail.com>
2017-01-23 16:01:21 -0800

    Modified server js to not assign a port

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
 
-//mongoose.connect();
+mongoose.connect('mongodb://localhost/workout')
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000);
+app.listen(process.env.port || 3000);
 
 // app.get('/', function() {
 //   res.send()
